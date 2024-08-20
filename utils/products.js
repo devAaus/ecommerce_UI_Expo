2555f7f65021b93b921eff43852dfd380f2f809a
Aayush Ghimire <aausgh@gmail.com>
2024-08-20 13:38:11 +0545

    feat: added products static data and ststic images

---
+++ utils/products.js
@@ -0,0 +1,94 @@
+import v2k from '../assets/shoes/v2k.png'
+import jordan_1_retro from '../assets/shoes/jordan_1_retro.png'
+import jordan_1_low from '../assets/shoes/jordan_1_low.png'
+import jordan_4_retro from '../assets/shoes/jordan_4_retro.png'
+import air_max_270 from '../assets/shoes/air_max_270.png'
+import offcourt_adjust from '../assets/shoes/offcourt_adjust.png'
+import jordan_1_g from '../assets/shoes/jordan_1_g.jpg'
+import air_force_1_low_retro from '../assets/shoes/air_force_1_low_retro.png'
+import air_force_1_07 from '../assets/shoes/air_force_1_07.png'
+import jordan_4_retro_oxidized from '../assets/shoes/jordan_4_retro_oxidized.png'
+
+
+export const products = [
+   {
+      id: 1,
+      name: 'Nike V2K Run',
+      description: "Fast forward. Rewind. Doesn't matter—this shoe takes retro into the future. The V2K remasters everything you love about the Vomero in a look pulled straight from an early '00s running catalogue. Layer up in a mixture of flashy metallics, breathable mesh and a midsole with a perfectly aged aesthetic. The dual-density foam midsole and chunky heel make sure wherever you go, it's in comfort.",
+      price: '120',
+      image: v2k,
+      favourite: false
+   },
+   {
+      id: 2,
+      name: 'Air Jordan 1 Retro High OG "Black and Gold"',
+      description: "The Air Jordan 1 Retro High remakes the classic sneaker, giving you a fresh look with a familiar feel. Premium materials with new colors and textures give modern expression to an all-time favorite.",
+      price: '180',
+      image: jordan_1_retro,
+      favourite: false
+   },
+   {
+      id: 3,
+      name: 'Air Jordan 1 Low OG "Wolf Grey"',
+      description: "The Air Jordan 1 Low OG remakes the classic sneaker with new colors and textures. Premium materials and accents give fresh expression to an all-time favorite.",
+      price: '140',
+      image: jordan_1_low,
+      favourite: false
+   },
+   {
+      id: '4',
+      name: 'Air Jordan 4 Retro "White Thunder"',
+      description: "Step into a classic. This AJ4 throws it back with full-grain and synthetic leathers and premium textiles. Lush colors and metallic pops update the icon, while original design elements—like floating eyestays and mesh-inspired accents—feel just as fresh as they did in '89.",
+      price: '215',
+      image: jordan_4_retro,
+      favourite: true
+   },
+   {
+      id: '5',
+      name: 'Nike Air Max 270',
+      description: "Nike's first lifestyle Air Max brings you style, comfort and big attitude in the Nike Air Max 270. The design draws inspiration from Air Max icons, showcasing Nike's greatest innovation with its large window and fresh array of colors.",
+      price: '160',
+      image: air_max_270,
+      favourite: true
+   },
+   {
+      id: '6',
+      name: "Nike Air Force 1 Low Retro",
+      description: "Got your fave color yet? No worries. The 'Color of the Month' series lets you feed your need while celebrating a little-known piece of Nike history—the original Color of the Month series, back in 1984, may have saved the Air Force 1 from extinction. That's right, a reissue was uncertain in the shoe's beginning, but sports shops in Baltimore saw them selling like hot cakes and knew the AF1 would become synonymous with street fashion and culture. So, they asked for custom colorways to sell from their storefronts...and the rest is history. From pristine materials to this crisp colorway (and even a shoe-cleaning brush), this edition delivers a lesson in coveted off-court style. Every time you rock 'em, be sure to think of the visionary shopkeepers in Baltimore that made it all happen.",
+      price: '150',
+      image: air_force_1_low_retro,
+      favourite: false
+   },
+   {
+      id: '7',
+      name: "Nike Air Force 1 '07",
+      description: "Comfortable, durable and timeless—it’s number 1 for a reason. The classic ‘80s construction pairs with bold details for style that tracks whether you’re on court or on the go. This basketball icon puts a fresh spin on what you know best: Durable materials, rich colors and the perfect amount of flash to make you shine.",
+      price: '120',
+      image: air_force_1_07,
+      favourite: true
+   },
+   {
+      id: '8',
+      name: 'Nike Offcourt Adjust',
+      description: "Post game, errands and more—adjust your comfort on the go. The adjustable, padded strap lets you perfect your fit, while the lightweight foam midsole brings first-class comfort to your feet.",
+      price: '40',
+      image: offcourt_adjust,
+      favourite: false
+   },
+   {
+      id: '9',
+      name: 'Air Jordan 4 Retro "Oxidized Green"',
+      description: "Step into a classic. This AJ4 throws it back with full-grain and synthetic leathers and premium textiles. Lush colors and metallic pops update the icon, while original design elements—like floating eyestays and mesh-inspired accents—feel just as fresh as they did in '89.",
+      price: '215',
+      image: jordan_4_retro_oxidized,
+      favourite: true
+   },
+   {
+      id: '10',
+      name: 'Air Jordan I High G',
+      description: "Feel unbeatable, from the tee box to the final putt. Inspired by one of the most iconic sneakers of all time, the Air Jordan 1 G is an instant classic on the course. With Air cushioning underfoot, a Wings logo on the heel and an integrated traction pattern to help you power through your swing, it delivers all the clubhouse cool of the original AJ1—plus everything you need to play 18 holes in comfort.",
+      price: '190',
+      image: jordan_1_g,
+      favourite: false
+   }
+]
